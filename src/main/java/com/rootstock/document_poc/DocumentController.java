9356a9bb6c9c9c525104fe6407678585b78888fc
Craig Gardner <craig.s.gardner@proton.me>
2025-09-09 12:56:39 -0700

    feat: handling file uploads to filesystem

--- src/main/java/com/rootstock/document_poc/DocumentController.java
+++ src/main/java/com/rootstock/document_poc/DocumentController.java
@@ -1,5 +1,6 @@
 package com.rootstock.document_poc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.codec.multipart.FilePart;
@@ -9,15 +10,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rootstock.document_poc.service.storage.StorageService;
+
+import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping(path = "/api/documents")
+@RequiredArgsConstructor
 public class DocumentController {
+
+    private final StorageService storageService;
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public Mono<ResponseEntity<String>> createDocument(@RequestPart("file") Mono<FilePart> file) {
-        return file.map(fp -> ResponseEntity.ok("Hello POST " + fp.filename()));
+        return file.flatMap(fp ->
+            storageService.store(fp)
+                .then(Mono.just(ResponseEntity.ok("Hello POST " + fp.filename())))
+        );
     }
 
     @GetMapping
---
+++ src/main/java/com/rootstock/document_poc/service/storage/StorageServiceException.java
@@ -0,0 +1,9 @@
+package com.rootstock.document_poc.service.storage;
+
+public class StorageServiceException extends Throwable {
+
+    public StorageServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
