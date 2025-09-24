cd19841a9e32e91ca51f9b7ac97854e4ef97d896
Craig Gardner <craig.s.gardner@proton.me>
2025-09-24 14:14:09 -0700

    feat: added file uploads

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,7 +1,7 @@
 import "@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.css"
 import "./App.css";
 import {useState} from 'react';
-import { Input, Button, Card, Spinner } from '@salesforce/design-system-react';
+import { Input, Button, Card, Spinner, ExpandableSection, IconSettings } from '@salesforce/design-system-react';
 import ky from 'ky';
 import { marked } from 'marked';
 
@@ -9,10 +9,10 @@ const tenantId = '123';
 
 function App() {
   const [query, setQuery] = useState<string>('');
-  // const [file, setFile] = useState<File | null>(null);
+  const [file, setFile] = useState<File | null>(null);
   const [conversation, setConversation] = useState<ConversationMessage[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  // const [isUploadOpen, setIsUploadOpen] = useState<boolean>(false);
+  const [isUploadOpen, setIsUploadOpen] = useState<boolean>(false);
   
   const api = ky.extend({
     hooks: {
@@ -26,54 +26,57 @@ function App() {
     setQuery(event.currentTarget.value);
   }
 
-  // const handleUpload = (_: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-  //   let formData = new FormData();
-  //   formData.append('file', file as Blob);
-  //   formData.append('tenantId', tenantId);
-  //   api.post('/api/documents', {
-  //     body: formData
-  //   })
-  // }
-
-  // const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   if (!event.currentTarget.files) {
-  //     return;
-  //   }
-  //   setFile(event.currentTarget.files[0]);
-  // }
-
   const handleSubmit = async (_: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     console.log(query);
     const response = await api.get(`/api/documents?question=${query}&tenantId=${tenantId}`)
-    setConversation([...conversation, {question: query, response: await response.text()}])
+    setConversation([{question: query, response: await response.text()}, ...conversation])
+    setQuery('')
+  }
+
+  const handleUpload = (_: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    let formData = new FormData();
+    formData.append('file', file as Blob);
+    formData.append('tenantId', tenantId);
+    api.post('/api/documents', {
+      body: formData
+    })
+  }
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!event.currentTarget.files) {
+      return;
+    }
+    setFile(event.currentTarget.files[0]);
   }
 
   return (
-    <div className="container">
-      <div className="left">
-        {/* <ExpandableSection id="file-upload" title="Upload a File" isOpen={isUploadOpen} onToggleOpen={() => setIsUploadOpen(!isUploadOpen)} classNames="flex-row"> */}
-        {/*   <Input type="file" label="File Upload" className="flex-column align-start" onChange={handleFileChange} /> */}
-        {/*   <Button label="Upload" className="flex-column align-end" onClick={handleUpload}/> */}
-        {/* </ExpandableSection> */}
-        <form>
-          <Input label="Query" onChange={handleInputChange} onSubmit={handleSubmit}/>
-          <Button label="Chat" variant="brand" onClick={handleSubmit} disabled={isLoading} />
-        </form>
-        <div>
-          File List will go here
+    <IconSettings iconPath="/assets/icons">
+      <div className="container">
+        <div className="left">
+          <ExpandableSection id="file-upload" title="Upload a File" isOpen={isUploadOpen} onToggleOpen={() => setIsUploadOpen(!isUploadOpen)} classNames="flex-row">
+            <Input type="file" label="File Upload" className="flex-column align-start" onChange={handleFileChange} />
+            <Button label="Upload" className="flex-column align-end" onClick={handleUpload}/>
+          </ExpandableSection>
+          <form>
+            <Input label="Query" onChange={handleInputChange} onSubmit={handleSubmit}/>
+            <Button label="Chat" variant="brand" onClick={handleSubmit} disabled={isLoading} />
+          </form>
+          <div>
+            File List will go here
+          </div>
         </div>
+        <div className="right">
+          {conversation.map((msg, index) => {
+              return (
+                  <Card id={index} heading={msg.question}>
+                      <div className="question-body" dangerouslySetInnerHTML={{ __html: marked.parse(msg.response) }}></div>
+                  </Card>
+              );
+          })}
+        </div>
+      {isLoading &&<Spinner size="large" variant="base" isVisible={isLoading} assistiveText={{ label: 'Loading' }} />}
       </div>
-      <div className="right">
-        {conversation.map((msg, index) => {
-            return (
-                <Card id={index} heading={msg.question}>
-                    <div className="question-body" dangerouslySetInnerHTML={{ __html: marked.parse(msg.response) }}></div>
-                </Card>
-            );
-        })}
-      </div>
-    {isLoading &&<Spinner size="large" variant="base" isVisible={isLoading} assistiveText={{ label: 'Loading' }} />}
-    </div>
+    </IconSettings>
   )
 }
 
